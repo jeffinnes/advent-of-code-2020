e793e40f46abdec5c6f93cf262e731914cc6c176
Jeff Innes <contact.me@jeffinnes.dev>
2020-12-05 14:59:54 -0700

    Day5 Start

---
+++ day5.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const dayFiveInput = 'inputs/day5-input.txt';
+const arrayOfSeats = fs.readFileSync(dayFiveInput, 'utf8').split('\n\n');
+
+function getRow(rowCode) {
+  
+}
+
+function getColumn(columnCode) {
+  
+}
+
+function getSeatID(rowNum, columnNum) {
+  
+}
